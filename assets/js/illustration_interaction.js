a6bb67375aa6bb249b5ef8fd3c89872759cd4f1b
Vivian <elizabeth.v.qu@gmail.com>
2019-06-18 10:03:39 +0900

    Enable hat picker

--- assets/js/illustration_interaction.js
+++ assets/js/illustration_interaction.js
@@ -7,6 +7,12 @@ const attachListeners = () => {
         }
         image.addEventListener('click', changeSelection, false);
     }
+
+    const flags = document.getElementsByClassName('flag');
+    for (var i = 0; i < flags.length; i++) {
+        let flag = flags[i];
+        flag.addEventListener('click', changeCountrySelection, false);
+    }
 }
 
 const getFileNameForImage = (image) => {
@@ -24,7 +30,6 @@ const getFileNameForImage = (image) => {
 const changeSelection = (e) => {
     let srcImage = e.srcElement;
     let animalPath = getFileNameForImage(srcImage);
-    let isSelected = srcImage.className.includes("illustrationImageSelected");
     if (animalPath == null || animalPath.length == 0) {
         return;
     }
@@ -45,6 +50,87 @@ const changeSelection = (e) => {
     if (updatedAnimal && animalName.length > 0) {
         const mainImage = document.getElementsByClassName('illustrationMainImage')[0];
         mainImage.src = 'assets/illustrations-main/' + animalName + '.png';
+        mainImage.className = "illustrationMainImage " + animalName;
+    }
+
+    const selectedCountries = document.getElementsByClassName('flag flagSelected');
+    if (selectedCountries.length == 0) {
+        return;
+    }
+    let flag = selectedCountries[0];
+    let country = getCountryFromFlag(flag);
+    updateAnimalHat(country, animalName);
+}
+
+const getCountryFromFlag = (image) => {
+    if (image.src == null || image.src.length == "") {
+        return;
+    }
+    let pathComponents = image.src.split('/');
+    if (pathComponents.length < 2) {
+        return "";
+    }
+
+    let imageName = pathComponents.slice(-1)[0];
+    let imageComponents = imageName.split("-");
+    return imageComponents[0];
+}
+
+const changeCountrySelection = (e) => {
+    let image = e.srcElement;
+    if (image == null) {
+        return;
+    }
+
+    var countrySelected = false;
+    if (image.className.includes("flagSelected")) {
+        image.className = image.className.replace(" flagSelected", " flagUnselected");
+    } else {
+        countrySelected = true;
+        image.className += " flagSelected";
+    }
+
+    const flags = document.getElementsByClassName('flag');
+    for (var i = 0; i < flags.length; i++) {
+        let flag = flags[i];
+        if (image != flag && flag.className.includes("flagSelected")) {
+            flag.className = flag.className.replace(" flagSelected", "")
+        }
+    }
+
+    if (countrySelected) {
+        let country = getCountryFromFlag(image);
+        const selectedAnimalImages = document.getElementsByClassName('illustrationImage illustrationImageSelected');
+        if (selectedAnimalImages.length == 0) {
+            return;
+        }
+        const animalPath = getFileNameForImage(selectedAnimalImages[0]);
+        let animalName = animalPath.split('-')[0];
+        updateAnimalHat(country, animalName);
+    } else {
+        removeAnimalHat();
     }
 }
+
+const updateAnimalHat = (country, animal) => {
+    const hatImages = document.getElementsByClassName('illustrationHat');
+    if (hatImages.length == 0) {
+        return;
+    }
+    let hatImage = hatImages[0];
+    hatImage.src = "assets/hats/" + animal + "-" + country + ".png";
+    hatImage.className = "illustrationHat " + animal + "-" + country + "-hat";
+}
+
+const removeAnimalHat = () => {
+    const hatImages = document.getElementsByClassName('illustrationHat');
+    if (hatImages.length == 0) {
+        return;
+    }
+    let hatImage = hatImages[0];
+    if (hatImage.className.includes('hidden') == false) {
+        hatImage.className += " hidden";
+    }
+}
+
 attachListeners();
